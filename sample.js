eaa6726504722c8f793390457454d77d74974efc
EC2 Default User <ec2-user@ip-172-31-41-20.ap-northeast-1.compute.internal>
2022-11-20 07:29:58 +0000

    [add]関数

--- sample.js
+++ sample.js
@@ -1,5 +1,10 @@
 // じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+//"グー"以外且つ、チョキ"以外且つ、"パー"以外且つ、キャンセルボタン以外だったら」というような条件を書く
+while ((user_hand != "グー")&&(user_hand != "チョキ")&&(user_hand != "パー")&&(user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+   user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
 
 // じゃんけんの手をランダムに作成する関数を呼び出す
 let js_hand = getJShand();
@@ -8,8 +13,11 @@ let js_hand = getJShand();
 let judge = winLose(user_hand, js_hand);
 
 // 結果を表示する
+if (user_hand != null){
 alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-
+}else{
+  alert('またチャレンジしてね');
+}
 // ランダムでじゃんけんの手を作成する関数
 function getJShand(){
   let js_hand_num = Math.floor(Math.random()* 3 );
